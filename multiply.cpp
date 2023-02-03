4ae44afa288e1bbf412cfde76631d7a122982507
tipx2 <tipx2gaming@gmail.com>
2023-02-03 15:20:18 +0000

    Create multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  std::cout << "Enter two numeros" << std::endl;
+  std::cin >> x >> y;
+  std::cout << x << " times by " << y << " equals " << x*y << std::endl;
+  return 0;
+}
